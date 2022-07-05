7151e7c4d4ec5efac465582b8377e7b9ad8ef6ed
nafishahmeddev <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>
2022-07-05 22:19:37 +0530

    fix

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -44,7 +44,7 @@ class ProductModel extends Model {
     public function getProductDetails($product_id){
         $sql = "SELECT p.* FROM product p WHERE p.product_id=".$this->db->escape($product_id);
         $query = $this->db->query($sql);
-        return $query->getRowObject();
+        return $query->getRowArray();
     }
 
 
