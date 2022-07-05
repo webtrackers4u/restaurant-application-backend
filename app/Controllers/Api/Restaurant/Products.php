c87add4e5e3aef1bcfcb65612b93322ac52ea3a3
nafishahmeddev <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>
2022-07-05 21:46:24 +0530

    fix

--- app/Controllers/Api/Restaurant/Products.php
+++ app/Controllers/Api/Restaurant/Products.php
@@ -14,13 +14,6 @@ class Products extends ApiBaseController
         //search params
         $menu_id_s = $this->request->getGet("menu_id");
         $products = $productModel->getMenuProducts($menu_id_s);
-        $products = array_map(function ($product){
-            $product["thumbnail"] = $product["image_1"];
-            $product["banner"] = $product["image_2"];
-            unset($product["image_1"]);
-            unset($product["image_2"]);
-            return $product;
-        }, $products);
         return $this->response->setJSON([
             "result_code"=>200,
             "message"=>"Successful",
@@ -33,13 +26,6 @@ class Products extends ApiBaseController
         $productModel = new ProductModel();
         //search params
         $products = $productModel->getPopularProducts("restaurant");
-        $products = array_map(function ($product){
-            $product["thumbnail"] = $product["image_1"];
-            $product["banner"] = $product["image_2"];
-            unset($product["image_1"]);
-            unset($product["image_2"]);
-            return $product;
-        }, $products);
         return $this->response->setJSON([
             "result_code"=>200,
             "message"=>"Successful",
